ab2ee802d0d0948f49f92ca79f650279049d22b8
rgrandsire@gmail.com <598Th0mas>
2021-06-07 11:05:54 -0400

    Finished first draft

--- PresetVersionCheck/Form1.Designer.cs
+++ PresetVersionCheck/Form1.Designer.cs
@@ -28,13 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1006, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "E&xit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 270);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(251, 70);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Open and Check";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "*.mpt";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Presets|*.mpt|All Files|*.*";
+            this.openFileDialog1.Title = "Open Preset File";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1069, 180);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 213);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 38);
+            this.label1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(326, 270);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 70);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Clear All";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1093, 352);
+            this.ControlBox = false;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Preset Build Version Checker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- PresetVersionCheck/Form1.cs
+++ PresetVersionCheck/Form1.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿/*
+##############################################################################################################################################################
+#   Date      #  Version   #     Author        #                               Comments                                                                      #
+##############################################################################################################################################################
+# 06/03/2021  # 1.0.0.0    # Remi G Grandsire  #  Initial development                                                                                        #
+#_____________#____________#___________________#_____________________________________________________________________________________________________________#
+
+
+*/
+
+using System;
+using System.IO;
+using System.IO.Compression;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace PresetVersionCheck
 {
@@ -15,6 +22,92 @@ namespace PresetVersionCheck
         public Form1()
         {
             InitializeComponent();
+        }
+        private string zFile = string.Empty;
+        private string zipPath = "C:\\temp\\RemUnzip";
+
+
+        private string GetVersion(string xmlFile)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlFile);
+            return doc.OuterXml.ToString();
+        }
+        public static void Decompress(string filename, string outputfolder)
+        {
+            ZipStorer zip;
+
+            if (File.Exists(filename))
+                // Opens existing zip file
+                zip = ZipStorer.Open(filename, FileAccess.Read);
+            else
+                return;
+
+            // Read all directory contents
+            List<ZipStorer.ZipFileEntry> dir = zip.ReadCentralDir();
+
+            // Extract all files in target directory
+            string path;
+            bool result;
+            foreach (ZipStorer.ZipFileEntry entry in dir)
+            {
+                path = Path.Combine(outputfolder, entry.FilenameInZip);
+                result = zip.ExtractFile(entry, path);
+            }
+            zip.Close();
+        }
+        private void UnComp()
+        {
+            if (Directory.Exists(zipPath) != true)
+            {
+                Directory.CreateDirectory(zipPath);
+                AddLog("created folder: " + zipPath, listBox1);
+            }
+            else AddLog("Temp folder exists", listBox1);
+            AddLog("Preparing to uncompress: ", listBox1);
+            Decompress(zFile, zipPath);
+            AddLog("Uncompress completed", listBox1);
+        }
+        private static void AddLog(string zMess, ListBox listBox2)
+        {
+           listBox2.Items.Add(DateTime.Now.ToString()+"  :   "+ zMess);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                int start = 0;
+                int stop = 0;
+                string zResult = string.Empty;
+                string zVersion = string.Empty;
+                zFile = openFileDialog1.FileName;
+                AddLog("Preset file: " + zFile, listBox1);
+                AddLog("Opening Preset file for version check", listBox1);
+                UnComp();
+                zResult = GetVersion(zipPath + "\\Preset.xml");
+                AddLog(zResult, listBox1);
+                start = zResult.IndexOf("build");
+                stop = zResult.IndexOf("name");
+                zVersion = zResult.Substring(start, stop - start);
+                AddLog(zVersion, listBox1);
+                zVersion = zVersion.Substring(7, zVersion.Length -9);
+
+                label1.Text = "Preset Version: " + zVersion;    
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            zFile = string.Empty;
+            openFileDialog1.FileName = string.Empty;
+            label1.Text = string.Empty;
         }
     }
 }
